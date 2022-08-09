8896f071cceebc6d4f8baafc563de827af538dcf
Sayeednayeem1 <sayeedn850@gmail.com>
2022-08-09 21:11:30 +0600

    added codes

---
+++ declare-func.js
@@ -0,0 +1,5 @@
+function names(nam, nam1, nam2){
+    return nam + nam1 + nam2;
+}
+let name = names("nayeem", "sayeed", "whatever");
+console.log(name);
---
+++ parameter.js
@@ -0,0 +1,6 @@
+function numbers(number, number1){
+    let sum = number + number1;
+    return sum;
+}
+let number = numbers(34, 36);
+console.log(number);
