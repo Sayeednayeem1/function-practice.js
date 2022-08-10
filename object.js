454f5536e339713ea06fe534a1c4e38d591a9d17
Sayeednayeem1 <sayeedn850@gmail.com>
2022-08-10 09:56:35 +0600

    added new codes

---
+++ object.js
@@ -0,0 +1,17 @@
+let myTable = {
+    glass: 2,
+    laptop: "Asus",
+    keyboard: 2,
+    mouse: 1,
+    books: 10
+}
+console.log(myTable);
+console.log(myTable.books);
+let myBed = {
+    balish: 3,
+    bed: 2,
+    whatever: 4,
+    name: "superabed"
+}
+let key = Object.keys(myBed);
+console.log(key);
